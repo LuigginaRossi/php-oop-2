53610fca5668d61b1a14487e8bbe2f6eb686b1bb
LuigginaRossi <luigginavaldivia@gmail.com>
2023-01-16 19:21:09 +0100

    add cards

--- index.php
+++ index.php
@@ -2,14 +2,16 @@
 
 require_once "./classes/Product.php";
 require_once "./classes/Category.php";
+include "./db.php";
 
-$product= new Product("url.img", "name", 50, true, new Category("nomeCategoria", "icona"));
-var_dump($product);
+ $product1 = new Product("url.img", "name", 50, false, new Category("nomeCategoria", "icona"));
+ var_dump($product1);
+ echo "disponibile: " . $product1->getAvailable();
 
-echo "disponibile: " . $product->getAvailable();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -21,10 +23,33 @@ echo "disponibile: " . $product->getAvailable();
 
 
 </head>
+
 <body>
-    <h1 class="text-center py-5">Pet Shop</h1>
+    <h1 class="text-center pt-5">Pet Shop</h1>
     <!-- <span><i class="fa-solid fa-cat"></i><i class="fa-solid fa-dog"></i></span> -->
 
+    <div class="container py-4">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <?php foreach($productsList as $product){ ?>
+                <div class="col">
+                    <div class="card">
+                        <img src="imgs/<?php echo $product['img'] ?>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-dark"><?php echo "Product Name: ". $product['productName'] ?></h5>
+                            <p class="card-text">
+                                <?php echo  "Price: ". $product['price'] . "&euro;" ?><br>  
+                                <!-- echo  "Available: ".  $product->getAvailable() -->
+                                <?php echo  "Available: ". $product['available'] ?><br>    
 
+                                <?php echo  "Category: ". $product['categoryName']?>                              
+                                
+                                <span class="ps-3"><i class="fa-solid <?php echo $product['categoryIcon']?> "></i>
+                            </p>
+                        </div>   
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
 </body>
+
 </html>
